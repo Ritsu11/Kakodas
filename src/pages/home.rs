de0229c644392c7f2e95e50dfd4d8cc0d83000c3
ISHIMIGNE <garethbale3113@gmail.com>
2022-11-09 21:07:25 +0900

    11/9 v2

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,12 +1,13 @@
-use crate::router::route::Route;
+// use crate::router::route::Route;
 
 use gloo_net::http::Request;
 use serde::Deserialize;
 use yew::{html, Component, Context, Html};
-use yew_router::components::Link;
+// use yew_router::components::Link;
 
-use crate::components::home;
+// use crate::components::home;
 
+#[derive(Clone)]
 pub struct Home {
     json: Vec<Video>,
 }
@@ -28,36 +29,55 @@ impl Component for Home {
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        let mut jsons = Self { json: Vec::new() };
-        wasm_bindgen_futures::spawn_local(async move {
-            let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
-                .send()
-                .await
-                .unwrap()
-                .json()
-                .await
-                .unwrap();
-        });
+        let mut data: Vec<Video> = Vec::new();
+        {
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
 
-        jsons
+                for video in fetched_videos {
+                    &mut data.push(video.clone());
+                }
+            });
+        }
+
+        Self { json: vec![] }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Click => true,
+            Msg::Click => {
+                // console::log_1(&JsString::from("hello".to_string()));
+                true
+            }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let link = ctx.link();
 
-        html! {
-            <>
-                <home::Home />
-                <button onclick={link.callback(|_| Msg::Click)}>{"show"}</button>
-                <div><Link<Route> to={Route::Login}>{ "click here to go Login" }</Link<Route>></div>
-                <div><Link<Route> to={Route::Register}>{ "click here to go Register" }</Link<Route>></div>
-            </>
-        }
+        self.json
+            .iter()
+            .map(|video| {
+                html! {
+                    <p>{format!("{}: {}", video.speaker, video.title)}</p>
+                }
+            })
+            .collect()
+
+        // html! {
+        //     <>
+        //         // <home::Home />
+
+        //         <button onclick={link.callback(|_| Msg::Click)}>{"show"}</button>
+        //         <div><Link<Route> to={Route::Login}>{ "click here to go Login" }</Link<Route>></div>
+        //         <div><Link<Route> to={Route::Register}>{ "click here to go Register" }</Link<Route>></div>
+        //     </>
+        // }
     }
 }
